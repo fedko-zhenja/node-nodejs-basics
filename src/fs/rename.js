8185fa33c7e0c2f058ad8e312037533971d7a829
@fedko-zhenja <fedkozhenja@gmail.com>
2024-01-23 22:16:19 +0400

    feat: implement function for rename.js file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
+
+const pathWrongFile = 'src/fs/files/wrongFilename.txt';
+const pathProperFile = 'src/fs/files/properFilename.md'; 
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        await fs.access(pathProperFile);
+        console.error('FS operation failed'); 
+    } catch {
+        try {
+            await fs.access(pathWrongFile);
+            await fs.rename(pathWrongFile, pathProperFile);
+        } catch {
+            console.error('FS operation failed'); 
+        }
+    }
 };
 
 await rename();
