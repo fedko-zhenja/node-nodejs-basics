c2a56caf2a669380582e92907954d792c2f12f36
@fedko-zhenja <fedkozhenja@gmail.com>
2024-01-23 21:57:26 +0400

    feat: implement function for create.js file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
-const create = async () => {
-    // Write your code here 
-};
-
+import fs from 'fs/promises'; 
+ 
+const path = 'src/fs/files/fresh.txt'; 
+const content = 'I am fresh and young'; 
+ 
+const create = async () => { 
+    try { 
+        await fs.access(path); 
+        console.error('FS operation failed'); 
+    } catch { 
+        try { 
+            await fs.writeFile(path, content); 
+        } catch (error) { 
+            console.error(error); 
+        } 
+    } 
+}; 
+ 
 await create();
