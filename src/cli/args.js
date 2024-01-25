1e09995fd496bc1181e9e8d52bb5b8161dc99479
@fedko-zhenja <fedkozhenja@gmail.com>
2024-01-25 23:05:18 +0400

    feat: implement function for args.js file

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    let resaltStr = '';
+
+    process.argv.forEach((el, index) => {
+        if(el.startsWith('--')) {
+            index === process.argv.length - 2 ?
+                resaltStr += `${el.slice(2)} is ${process.argv[index + 1]}` : 
+                resaltStr += `${el.slice(2)} is ${process.argv[index + 1]}, `
+        }
+    });
+
+    console.log(resaltStr);
+
 };
 
 parseArgs();
