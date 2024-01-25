1f1374e5ca0195b5b82d77e714ab64edca20bf2d
@fedko-zhenja <fedkozhenja@gmail.com>
2024-01-25 22:05:32 +0400

    feat: implement function for env.js file

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
 const parseEnv = () => {
-    // Write your code here 
+    const processEnvArray = Object.entries(process.env);
+    const RSS_array = processEnvArray.filter((arr) => arr[0].startsWith('RSS_'))
+    let RSS_resaltStr = '';
+
+    RSS_array.forEach((arr, index) => {
+        if(index === RSS_array.length - 1) {
+            RSS_resaltStr += `${arr[0]}=${arr[1]}`
+        } else {
+            RSS_resaltStr += `${arr[0]}=${arr[1]}; `
+        }
+    })
+
+    console.log(RSS_resaltStr);
 };
 
 parseEnv();
