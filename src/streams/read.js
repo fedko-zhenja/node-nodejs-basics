bf50b82b0e07f10e120e2d70e92c22aa58eab7b3
@fedko-zhenja <fedkozhenja@gmail.com>
2024-01-26 17:45:44 +0400

    feat: implement function for read.js file in sreams folder

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pathFile = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const fileStream = fs.createReadStream(pathFile);
+    fileStream.pipe(process.stdout);
 };
 
 await read();
