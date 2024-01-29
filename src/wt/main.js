bda96bd974bb7333c34f418c99828f05fb065098
@fedko-zhenja <fedkozhenja@gmail.com>
2024-01-29 20:09:43 +0400

    feat: implement functions for main.js and worker.js files

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
+import { Worker } from 'node:worker_threads';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { cpus } from 'node:os';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pathWorkerFile = path.join(__dirname, 'worker.js');
+
 const performCalculations = async () => {
-    // Write your code here
+    const promisesWorkersArray = [];
+
+    for (let i = 0; i < cpus().length; i++) {
+        promisesWorkersArray.push(
+            new Promise((res) => {
+                const worker = new Worker(pathWorkerFile, { workerData: i + 10 });
+
+                worker.on('message', (data) => res({
+                    status: 'resolved',
+                    data
+                }));
+
+                worker.on('error', () => res({
+                    status: 'error',
+                    data: null
+                }));
+            })
+        );
+    }
+    
+    const promisesWorkersRes = await Promise.all(promisesWorkersArray);
+    console.log(promisesWorkersRes);
 };
 
 await performCalculations();
